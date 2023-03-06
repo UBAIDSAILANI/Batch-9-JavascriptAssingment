7d082afefaddc3da299a7a5c4721a13282cc35c9
Ubaid ur Rehman <77199751+UBAIDSAILANI@users.noreply.github.com>
2023-03-06 21:37:00 +0500

    Journey Start

---
+++ Chapter 1/app.js
@@ -0,0 +1,22 @@
+// Question 1
+
+var name = " Ubaid ur Rehman";
+alert('Hello' + name);
+
+var name = " Ubaid ur Rehman";
+swal('Hello' + name);
+
+// question 2
+
+alert("Error! Enter A valid Password");
+
+// question 3
+alert("Welcome to Java Land \n Happy Coding!" )
+// question 4
+
+// question realate to all above questions from 1 to 3
+// question 5
+// written in browser
+// question 6
+// question 7
+// all above question are cleared
