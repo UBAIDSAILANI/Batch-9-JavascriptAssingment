2bef5666d709d83c8e013f6a14e0bab6d22f0d1c
Ubaid ur Rehman <77199751+UBAIDSAILANI@users.noreply.github.com>
2023-03-06 21:39:45 +0500

    Journey Start

---
+++ Chapter 2/app.js
@@ -0,0 +1,33 @@
+
+swal("Hello world!");
+// question 1
+var userName;
+// question 2
+var myName = "Ubaid ur Rehman";
+// question 3
+// a
+var message;
+// b
+message = "Hello World";
+// c
+
+alert(message);
+// question 4
+var name = "ubaid";
+var age = 33 + " Years old";
+var course = "certified mobile application development";
+(name);
+alert(age);
+alert(course);
+// question 5
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(pizza)
+// question 6
+var email = "ubaidsailani@gmail.com"
+alert("Your Email is " + email);
+// question 7
+var book = "A Smarter way to learn Javascript";
+alert("i am trying to learn " + book);
+// question 8
+var newMessage = "Yah! I can write HTML content through JavaScript"
+document.write(newMessage)
