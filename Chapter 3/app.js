efaee07d2aa5308c38f1ef04d8d5056dab0255df
Ubaid ur Rehman <77199751+UBAIDSAILANI@users.noreply.github.com>
2023-03-06 21:40:44 +0500

    Add files via upload

---
+++ Chapter 3/app.js
@@ -0,0 +1,16 @@
+// q#1
+var age = 15;
+alert("I am " +age+ " years old");
+// q#2
+
+
+// Q#3
+var birthYear = 1991;
+document.write("My Birth Year is " +birthYear ); 
+"\n" + document.write("Data type of My decleared variable is "+ typeof(birthYear))
+// q#4
+var visitorName = "John Doe"
+var productTitle = "T-Shirts"
+var quantity = +prompt("How many shirts want to order");
+document.write("<b>"+  visitorName  +"</b>" + " ordered " + "<b>" + quantity +"</b>" +" " + productTitle + " on xyz's Clothing");
+
