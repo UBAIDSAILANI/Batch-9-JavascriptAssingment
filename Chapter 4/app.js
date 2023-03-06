07f6d195bfd376ae048a05b75276ee1fbff2ded6
Ubaid ur Rehman <77199751+UBAIDSAILANI@users.noreply.github.com>
2023-03-06 21:42:48 +0500

    Journey Start

---
+++ Chapter 4/app.js
@@ -0,0 +1,16 @@
+// q#1
+var one = "first";
+one = "two";
+one = 'three';
+alert(one);
+// q#2
+var legalVariable = "name, $name, _name, nameOf, name234,name_of";
+var illegalVariable = "12name, 1223, jsKeyWord, ";
+// Q#3
+var heading = "<b>" + "Rules for naming Js Variables " +"</b>"
+document.write(heading);
+// Q#4
+var name = "camelCase, snake_case, $sign, var123";
+alert(name);
+var start = "variable start with $name, _name, name" + "variable are case sensitive" + "variable should not be Js KeyWord";
+alert(start);
